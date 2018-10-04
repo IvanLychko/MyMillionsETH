ec3a4f2e76ba2577265f9febe8ba01dfc33eb25f
Alexey Shagaleev <alexey.shagaleev@yandex.ru>
2018-10-04 13:45:51 +0500

    add register tests and data view functions

--- contracts/MyMillions.sol
+++ contracts/MyMillions.sol
@@ -5,12 +5,15 @@ import "./SafeMath.sol";
 
 contract MyMillions is Ownable {
     using SafeMath for uint256;
+
+    event CreateUser(uint256 _index, address _address, uint256 _balance);
+    event ReferralRegister(uint256 _refferalId, uint256 _userId);
     
     struct User {
         address addr;           // user address
         uint256 balance;        // balance of account
         uint256 totalPay;       // sum of all input pay
-        uint256[] referrals;    // first layer referrals
+        uint256[] referrals;    // first layer referrals ids
     }
     
     User[] public users;
@@ -29,30 +32,40 @@ contract MyMillions is Ownable {
         users.push(User(0x0, 0, 0, new uint256[](0)));  // for find by addressToUser map
     }
     
+    /**
+     * @dev register for only new users with min pay
+     */
     function register() public payable returns(uint256) {
         require(addressToUser[msg.sender] == 0);
         
-        User memory user = User(msg.sender, 0, 0, new uint256[](0));
+        User memory user = User(msg.sender, msg.value, msg.value, new uint256[](0));
         uint256 index = users.push(user) - 1;
         addressToUser[msg.sender] = index;
-        
+
+        emit CreateUser(index, msg.sender, msg.value);
         return index;
     }
+
     /**
-     * @dev essentially the same as buy, but instead of you sending ether 
-     * from your wallet, it uses your unwithdrawn earnings.
-     * -functionhash- 0x349cdcac (using ID for affiliate)
-     * -functionhash- 0x82bfc739 (using address for affiliate)
-     * -functionhash- 0x079ce327 (using name for affiliate)
+     * @dev just registry by referral link
      * @param _refId the ID of the user who gets the affiliate fee
      */
-    function register(uint256 _refId) public payable returns(uint256) {
+    function registerWithRefID(uint256 _refId) public payable returns(uint256) {
         require(_refId < users.length);
         
         uint256 index = register();
         users[_refId].referrals.push(index);
-        
+
+        emit ReferralRegister(_refId, index);
         return index;
+    }
+
+    function referralsOf() public view returns (uint256[]) {
+        return users[addressToUser[msg.sender]].referrals;
+    }
+
+    function balanceOf() public view returns (uint256) {
+        return users[addressToUser[msg.sender]].balance;
     }
     
     function buyWoodFactory() public payable {
--- test/MyMillions.js
+++ test/MyMillions.js
@@ -13,14 +13,70 @@ contract('MyMillions', function(accounts) {
     let myMillions;
 
     const owner = accounts[0];
+    const user0 = accounts[1];
+    const user1 = accounts[2];
+    const user2 = accounts[3];
 
     const gasPrice = web3.toWei('15', 'gwei');
 
     beforeEach('setup contract for each test', async function () {
         myMillions = await MyMillions.new({from: owner});
-    }); 
+    });
 
     it('has an owner', async function () {
         expect(await myMillions.owner()).to.equal(owner);
+    });
+
+    it('register users', async function () {
+        // register user with index 1
+        let user0_id = 1;
+        await myMillions.register({from: user0});
+
+        let user0_info = await myMillions.users(user0_id);
+        expect(user0_info[0]).to.equal(user0);
+
+        // register user with index 2
+        let user1_id = 2;
+        await myMillions.register({from: user1});
+
+        let user1_info = await myMillions.users(user1_id);
+        expect(user1_info[0]).to.equal(user1);
+    });
+
+    it('register users again', async function () {
+        // register user
+        let user0_id = 1;
+        await myMillions.register({from: user0});
+
+        // register user again
+        await expectThrow(myMillions.register({from: user0}));
+    });
+
+    it('register users with ref id', async function () {
+        // register user with index 1
+        let user0_id = 1;
+        await myMillions.register({from: user0});
+
+        // register user with refId of first user
+        let user1_id = 2;
+        await myMillions.registerWithRefID(user0_id, {from: user1});
+
+        // register user with refId of first user
+        let user2_id = 3;
+        await myMillions.registerWithRefID(user0_id, {from: user2});
+
+        let user0_referrals = await myMillions.referralsOf({from: user0});
+        expect(user0_referrals[0].toNumber()).to.equal(user1_id);
+        expect(user0_referrals[1].toNumber()).to.equal(user2_id);
+    });
+
+    it('register with initial balance', async function () {
+        // register user with index 1
+        let sum = 10000;
+        let user0_id = 1;
+        await myMillions.register({from: user0, value: sum});
+
+        let user0_balance = (await myMillions.balanceOf({from: user0})).toNumber();
+        expect(user0_balance).to.equal(sum);
     });
 });
