989c757ba62f37cd014e315374112cb2c1659345
Alexey Shagaleev <alexey.shagaleev@yandex.ru>
2018-10-19 02:00:47 +0500

    add math library

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,8 @@
 var MyMillions = artifacts.require("./MyMillions.sol");
+var Math = artifacts.require("./Math.sol");
 
 module.exports = function (deployer) {
-	deployer.deploy(MyMillions);
+    deployer.deploy(Math);
+    deployer.link(Math, MyMillions);
+    deployer.deploy(MyMillions);
 };
